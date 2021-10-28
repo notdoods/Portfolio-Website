6ab0937ea925c5e7d70314c8fa04689a377f7537
Kevin Chao <khchao@uci.edu>
2021-10-28 09:01:42 -0700

    Email

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,6 @@ import ContainerBlock from "../components/ContainerBlock";
 import Contact from "../components/Contact";
 
 export default function contact() {
-  require("dotenv").config();
   return (
     <ContainerBlock>
       <Contact />
